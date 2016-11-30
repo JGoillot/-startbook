937756abc0ea87a5ba48c6f09b33162c7fc7ae26
JFachetti <goillot.jeremy@gmail.com>
2016-11-30 09:27:12 +0100

    Amazon Cover scrapper

---
+++ app/services/image_amazon.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+class ImageAmazon < ServiceBase
+  attr_accessor :book
+
+  def initialize(book)
+    @book = book
+  end
+
+  def run
+    keyword_to_cover(@book)
+  end
+
+  private
+
+  def keyword_to_cover(book)
+    url = "https://www.amazon.fr/s/&field-keywords=#{book}"
+    html_file = open(url)
+    html_doc = Nokogiri::HTML(html_file)
+
+    html_doc.text
+  end
+end
