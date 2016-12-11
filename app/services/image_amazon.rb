12011bb16bf5f75a516b03be04c8c1df73b14904
JFachetti <goillot.jeremy@gmail.com>
2016-12-11 18:04:03 +0100

    Cards for book first div :art:

--- app/services/image_amazon.rb
+++ app/services/image_amazon.rb
@@ -8,17 +8,26 @@ class ImageAmazon < ServiceBase
   end
 
   def run
-    keyword_to_cover(@book)
+    keyword_to_data(@book)
   end
 
   private
 
-  def keyword_to_cover(book)
-    url = "https://www.amazon.fr/s/&field-keywords=#{book}"
+  def keyword_to_data(book)
+    book = URI.encode(I18n.transliterate(book))
+    keywork_book = book.gsub(" ","+")
+    url = "https://www.amazon.fr/s/&field-keywords=#{keywork_book}"
     html_file = open(url)
     html_doc = Nokogiri::HTML(html_file)
 
-    puts "lol"
-    # html_doc.text
+    puts amazon_url = html_doc.css('#s-results-list-atf').css('li a')[0]['href']
+
+    html_file = open(amazon_url)
+    html_doc = Nokogiri::HTML(html_file)
+
+    cover_link = html_doc.css('#img-canvas img').attr('src')
+    puts cover_url = cover_link.to_s.gsub("_SY","_SX")
+
+    puts book_rate = html_doc.css('#reviewStarsLinkedCustomerReviews').text.to_s
   end
 end
