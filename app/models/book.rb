b5540f3758e1086591365b59f34619fd18777408
JFachetti <goillot.jeremy@gmail.com>
2016-12-16 17:18:20 +0100

    Cover Book :tada:

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,8 +2,9 @@ class Book < ApplicationRecord
 
   def picture
     if cover_url.blank?
-      image_amazon = ImageAmazon.run(name)
+      image_amazon = ImageAmazon.run(self.title)
       self.cover_url = image_amazon
+      self.save
     end
     self.cover_url
   end
--- app/services/image_amazon.rb
+++ app/services/image_amazon.rb
@@ -20,14 +20,14 @@ class ImageAmazon < ServiceBase
     html_file = open(url)
     html_doc = Nokogiri::HTML(html_file)
 
-    puts amazon_url = html_doc.css('#s-results-list-atf').css('li a')[0]['href']
+    amazon_url = html_doc.css('#s-results-list-atf').css('li a')[0]['href']
 
     html_file = open(amazon_url)
     html_doc = Nokogiri::HTML(html_file)
 
     cover_link = html_doc.css('#img-canvas img').attr('src')
-    puts cover_url = cover_link.to_s.gsub("_SY","_SX")
+    return cover_url = cover_link.to_s.gsub("_SY","_SX")
 
-    puts book_rate = html_doc.css('#reviewStarsLinkedCustomerReviews').text.to_s
+    # puts book_rate = html_doc.css('#reviewStarsLinkedCustomerReviews').text.to_s
   end
 end
